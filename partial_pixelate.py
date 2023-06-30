84bfe4c5682f1df44cd758f3d4aa12334f529486
dvtiendat <111187020+dvtiendat@users.noreply.github.com>
2023-06-30 16:37:05 +0700

    update partial pixelate

--- partial_pixelate.py
+++ partial_pixelate.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("image/kingguin.jpg")
+img = cv2.imread("image/people.jpg")
 img = cv2.resize(img, (600, 600))
 HEIGHT, WIDTH = img.shape[:2]
 pixel_h, pixel_w = (15, 15)
 
-rg_x, rg_y, r_width, r_height = 100, 100, 200, 200
+rg_x, rg_y, r_width, r_height = 170, 50, 200, 200
 
 output = img.copy()
 partial = output[rg_y:rg_y+r_height, rg_x:rg_x+r_width]
